33d29da6ec7911e885150e296b9cae4622002bb1
Sharjeel-Akram <sharjeel.akram0200@gmail.com>
2023-11-14 01:22:59 +0500

    initial commit

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectToMongo = () => {
+    mongoose.connect("mongodb://127.0.0.1/mynotebook")
+        .then(() => {
+            console.log('Connected to MongoDB successfully');
+        })
+        .catch((err) => {
+            console.error('Error connecting to MongoDB:', err);
+        });
+};
+module.exports = connectToMongo;
